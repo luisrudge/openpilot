60c955cb27397cc7fb9cfc0ebce72116c60574e9
Cameron Clough <cameronjclough@gmail.com>
2024-03-18 17:14:32 +0000

    updates

--- selfdrive/car/psa/carstate.py
+++ selfdrive/car/psa/carstate.py
@@ -1,6 +1,5 @@
 from cereal import car
 from opendbc.can.parser import CANParser
-from openpilot.common.conversions import Conversions as CV
 from openpilot.selfdrive.car.psa.psacan import CanBus
 from openpilot.selfdrive.car.psa.values import DBC, CarControllerParams
 from openpilot.selfdrive.car.interfaces import CarStateBase
@@ -20,18 +19,17 @@ class CarState(CarStateBase):
       cp.vl['Dyn4_FRE']['P265_VehV_VPsvValWhlBckL'],
       cp.vl['Dyn4_FRE']['P266_VehV_VPsvValWhlBckR'],
     )
-    # ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr]) * CV.KPH_TO_MS
-    ret.vEgoRaw = cp.vl['Dyn_ABR']['P010_Com_v']  # or cp.vl['Dyn_Veh']['P060_vECU']
+    ret.vEgoRaw = cp.vl['Dyn_ABR']['P010_Com_v']  # also ['HS2_DYN_ABR_38D']['VITESSE_VEHICULE_ROUES']
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.yawRate = cp.vl['Dyn2_FRE']['YAW_RATE'] * CV.DEG_TO_RAD
+    # ret.yawRate = cp.vl['Dyn2_FRE']['YAW_RATE'] * CV.DEG_TO_RAD  # FIXME
     ret.standstill = False  # TODO
 
     # gas
-    ret.gas = cp.vl['DRIVER']['GAS_PEDAL'] / 99.5
+    ret.gas = cp_adas['HS2_BGE_DYN5_CMM_228']['EFCMNT_PDLE_ACCEL'] / 99.5
     ret.gasPressed = ret.gas > 1e-6  # TODO
 
     # brake
-    ret.brake = cp.vl['Dyn2_FRE']['BRAKE_PRESSURE'] / 1500.
+    # ret.brake = cp.vl['Dyn2_FRE']['BRAKE_PRESSURE'] / 1500.  # FIXME
     ret.brakePressed = bool(cp.vl['Dat_BSI']['P013_MainBrake'])
     ret.parkingBrake = bool(cp.vl['Dat_BSI']['PARKING_BRAKE'])  # TODO: check e-brake
 
@@ -42,7 +40,7 @@ class CarState(CarStateBase):
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > CarControllerParams.STEER_DRIVER_ALLOWANCE, 5)  # TODO: adjust threshold
     ret.steerFaultTemporary = 0  # TODO
     ret.steerFaultPermanent = 0  # TODO
-    ret.espDisabled = 0  # TODO
+    ret.espDisabled = False  # TODO
 
     # TODO: cruise state
     ret.cruiseState.speed = 0
@@ -82,12 +80,13 @@ class CarState(CarStateBase):
   @staticmethod
   def get_can_parser(CP):
     messages = [
-      ('WHEEL_SPEEDS', 50),
-      ('DYNAMICS', 100),
-      ('DRIVER', 10),
-      ('BODY', 20),
-      ('BODY_2', 50),
-      ('STEERING_COLUMN', 10),
+      ('Dyn4_FRE', 50),
+      ('Dyn_ABR', 25),
+      ('HS2_BGE_DYN5_CMM_228', 100),
+      ('Dat_BSI', 20),
+      ('STEERING_ALT', 100),
+      ('STEERING', 100),
+      ('Dyn2_CMM', 50),
       ('RESTRAINTS', 10),
     ]
     return CANParser(DBC[CP.carFingerprint]['pt'], messages, CanBus(CP).main)
